96f5e9152ac7ef27d048933c5da63fe2e2ecf093
MrSnake0208 <yangpeide0208@gmail.com>
2025-12-15 18:32:11 +0800

    feat: 增加录制功能

--- MFAAvalonia/App.axaml.cs
+++ MFAAvalonia/App.axaml.cs
@@ -115,6 +115,7 @@ public partial class App : Application
             // Add pages
             .AddView<Views.Pages.CopilotView, ViewModels.Pages.CopilotViewModel>(services)
             .AddView<TaskQueueView, TaskQueueViewModel>(services)
+            .AddView<RecordTaskView, RecordTaskViewModel>(services)
             .AddView<ResourcesView, ResourcesViewModel>(services)
             .AddView<SettingsView, SettingsViewModel>(services)
             .AddView<ScreenshotView, ScreenshotViewModel>(services)
