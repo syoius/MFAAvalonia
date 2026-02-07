7029c229113d8487f3cdf85edcc6b08b70296c32
Mr Snake <yangpeide0208@gmail.com>
2026-02-07 17:19:47 +0800

    fix: 为ObservableQueue类增加线程安全保护

--- MFAAvalonia/Helper/ValueType/ObservableQueue.cs
+++ MFAAvalonia/Helper/ValueType/ObservableQueue.cs
@@ -8,6 +8,7 @@ namespace MFAAvalonia.Helper.ValueType;
 public partial class ObservableQueue<T> : ObservableObject
 {
     private readonly Queue<T> _queue = new();
+    private readonly object _lock = new();
 
     [ObservableProperty] private int _count;
 
@@ -24,35 +25,54 @@ public partial class ObservableQueue<T> : ObservableObject
     
     public void Enqueue(T task)
     {
-        _queue.Enqueue(task);
-        Count = _queue.Count;
+        lock (_lock)
+        {
+            _queue.Enqueue(task);
+            Count = _queue.Count;
+        }
     }
 
     public T Dequeue()
     {
-        var task = _queue.Dequeue();
-        Count = _queue.Count;
-        return task;
+        lock (_lock)
+        {
+            var task = _queue.Dequeue();
+            Count = _queue.Count;
+            return task;
+        }
     }
 
     public void Clear()
     {
-        _queue.Clear();
-        Count = _queue.Count;
+        lock (_lock)
+        {
+            _queue.Clear();
+            Count = _queue.Count;
+        }
     }
+
     public bool Any()
     {
-        return _queue.Any();
+        lock (_lock)
+        {
+            return _queue.Any();
+        }
     }
-    
+
     public bool Any(Func<T, bool> predicate)
     {
-        return _queue.Any(predicate);
+        lock (_lock)
+        {
+            return _queue.Any(predicate);
+        }
     }
 
     public int CountWhere(Func<T, bool> predicate)
     {
-        return _queue.Count(predicate);
+        lock (_lock)
+        {
+            return _queue.Count(predicate);
+        }
     }
     
     public class CountChangedEventArgs(int oldValue, int newValue) : EventArgs
